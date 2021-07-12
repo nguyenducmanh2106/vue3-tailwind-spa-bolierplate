9d77604d6493909b4a29618e1c83ea8897fccc92
Md. Rakibul Islam <rakib1708@gmail.com>
2021-07-12 21:16:22 +0600

    PosCSS Issue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mx-auto">
+    <div class="container mx-auto ">
         <div class="flex mt-24 w-100 justify-center">
             <div class="flex justify-center items-center">
                 <img class="object-center h-24 w-100 m-3" src="/images/logo.png"/>
@@ -8,7 +8,7 @@
             </div>
             <div class="w-30rem pl-4 ">
                 <h2 class="text-3xl mb-5 font-bold">Vue 3 & Tailwind Boilerplate</h2>
-                <p class=" mb-3  text-gray-500">A single page application boilerplate for quick starting your Vue 3 app with Vue.js and Tailwind CSS. Get started <a href="https://github.com/rakibdevs/vue3-tailwind-spa-bolierplate.git" class="text-red-400">here</a></p>.
+                <p class=" mb-3  text-gray-500">A single page application boilerplate for quick starting your Vue 3 app with Vue.js and Tailwind CSS. Get started <a href="https://github.com/rakibdevs/vue3-tailwind-spa-bolierplate" class="text-red-400">here</a></p>.
                 <div class="inline-block mt-3">
                     <span class="text-xs px-3 bg-green-200 text-green-800 rounded-full m-1">vue-router</span>
                     <span class="text-xs px-3 bg-green-200 text-green-800 rounded-full m-1"> vuex</span>
@@ -18,5 +18,6 @@
                 
             </div>
         </div>
+        <p class="mt-24 w-100 text-center">Contributed by <a href="https://rakibul.dev" class="text-green-600">@Md. Rakibul Islam</a></p>
     </div>
 </template>
